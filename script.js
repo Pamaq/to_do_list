d426ef96a6ddd08e35dbaa30a1bea6442705cd29
Pamaq <paulinakuta00@gmail.com>
2022-10-14 21:52:19 +0200

    Added tools buttons and function

--- script.js
+++ script.js
@@ -31,4 +31,19 @@ const addNewTask = () => {
 	}
 };
 
+const createToolsArea = () => {
+	const divTools = document.createElement("div");
+	divTools.classList.add("tools");
+	const completeBtn = document.createElement("button");
+	completeBtn.classList.add("complete");
+	completeBtn.innerHTML = <i class="fas fa-check"></i>;
+	const editBtn = document.createElement("button");
+	editBtn.classList.add("edit");
+	editBtn.textContent = "EDIT";
+	const deleteBtn = document.createElement("button");
+	deleteBtn.classList.add("delete");
+	deleteBtn.innerHTML = <i class="fas fa-times"></i>;
+	divTools.append(completeBtn, editBtn, deleteBtn);
+};
+
 document.addEventListener("DOMContentLoaded", main); //event odpowiadajacy za wczytanie sie calej strony przed uruchomieniem skryptow
