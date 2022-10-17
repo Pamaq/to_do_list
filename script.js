3fd9c0451f2b40448a101f38d9d70c3ba567268f
Pamaq <paulinakuta00@gmail.com>
2022-10-17 15:26:18 +0200

    Allowed editing and deleting tasks

--- script.js
+++ script.js
@@ -20,10 +20,18 @@ const prepareDOMElements = () => {
 	errorInfo = document.querySelector(".error-info");
 	addBtn = document.querySelector(".btn-add");
 	ulList = document.querySelector(".todolist ul");
+
+	popup = document.querySelector(".popup");
+	popupInfo = document.querySelector(".popup-info");
+	popupInput = document.querySelector(".popup-input");
+	popupAddBtn = document.querySelector(".accept");
+	popupCloseBtn = document.querySelector(".cancel");
 }; //pobieranie el
 const prepareDOMEvents = () => {
 	addBtn.addEventListener("click", addNewTask);
 	ulList.addEventListener("click", checkClick);
+	popupCloseBtn.addEventListener("click", closePopup);
+	popupAddBtn.addEventListener("click", changeTodoText);
 }; //nasluchiwanie
 
 const addNewTask = () => {
@@ -61,9 +69,38 @@ const checkClick = (e) => {
 		e.target.closest("li").classList.toggle("completed");
 		e.target.classList.toggle("completed");
 	} else if (e.target.matches(".edit")) {
-		console.log("edit");
+		editToDo(e);
 	} else if (e.target.matches(".delete")) {
-		console.log("delete");
+		deleteToDo(e);
 	}
 };
+
+const editToDo = (e) => {
+	todoToEdit = e.target.closest("li");
+	popupInput.value = todoToEdit.firstChild.textContent;
+
+	popup.style.display = "flex";
+};
+const closePopup = () => {
+	popup.style.display = "none";
+	popupInfo.textContent = "";
+};
+
+const changeTodoText = () => {
+	if (popupInput.value !== "") {
+		todoToEdit.firstChild.textContent = popupInput.value;
+		popup.style.display = "none";
+		popupInfo.textContent = "";
+	} else {
+		popupInfo.textContent = "Podaj jakas tresc!";
+	}
+};
+const deleteToDo = (e) => {
+	e.target.closest("li").remove();
+	const allTodos = ulList.querySelectorAll("li");
+	if (allTodos.length === 0) {
+		errorInfo.textContent = "Brak zadan na liscie";
+	}
+};
+
 document.addEventListener("DOMContentLoaded", main); //event odpowiadajacy za wczytanie sie calej strony przed uruchomieniem skryptow
